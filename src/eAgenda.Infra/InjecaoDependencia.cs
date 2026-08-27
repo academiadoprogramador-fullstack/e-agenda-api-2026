c5143eb1ec3c960548d821405c187d3889a30d82
Tiago Santini <66257666+tiagosantini@users.noreply.github.com>
2026-08-27 14:27:08 -0300

    Adiciona Identity no Domínio e Infraestrutura

--- src/eAgenda.Infra/InjecaoDependencia.cs
+++ src/eAgenda.Infra/InjecaoDependencia.cs
@@ -10,6 +10,7 @@ using eAgenda.Infra.Modulos.ModuloCompromisso;
 using eAgenda.Infra.Modulos.ModuloContato;
 using eAgenda.Infra.Modulos.ModuloDespesa;
 using eAgenda.Infra.Modulos.ModuloTarefa;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -26,6 +27,26 @@ public static class InjecaoDependencia
     )
     {
         services.AddSerilogLogger(configuration, logging);
+
+        services.AddDataProtection();
+
+        services.AddIdentityCore<IdentityUser<Guid>>(options =>
+        {
+            options.User.RequireUniqueEmail = true;
+            options.SignIn.RequireConfirmedEmail = false;
+            options.Password.RequiredLength = 8;
+            options.Password.RequireDigit = true;
+            options.Password.RequireNonAlphanumeric = true;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireLowercase = false;
+            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            options.Lockout.MaxFailedAccessAttempts = 5;
+            options.Lockout.AllowedForNewUsers = true;
+        })
+        .AddRoles<IdentityRole<Guid>>()
+        .AddEntityFrameworkStores<EAgendaDbContext>()
+        .AddSignInManager()
+        .AddDefaultTokenProviders();
 
         services.AddDbContext<EAgendaDbContext>(options =>
        {
